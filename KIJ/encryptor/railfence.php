f3d4c414d4035cf17c4b73e8fcc65cbcd1631b0e
IlhamYanto <mohilhamfebriyanto.7a.14@gmail.com>
2018-03-23 06:07:44 +0700

    Rail Fence Web diperbaharui

--- KIJ/encryptor/railfence.php
+++ KIJ/encryptor/railfence.php
@@ -20,7 +20,6 @@
 </style>
 <head>
 	<title>Reilfence</title>
-	<!-- <script src="Encryptorr_files/encryptor.js"></script> -->
 </head>
 <body>
 	<div align="center"><h2><b>Reilfence</b></h2></div>
@@ -31,6 +30,10 @@
 				<td><label for="plain">Text:</label></td>
 				<td><textarea id="plain" cols="50" rows="10" style="width:40em; height:15em">inienkripsi</textarea></td>
 			</tr>
+      <tr>
+        <td ><label for="key">Depth:</label></td>
+        <td><input type="text" id="key" value=3 style="width:4em;"/></td>
+      </tr>
 			<tr>
 				<td></td>
 				<td>
@@ -56,97 +59,89 @@
 	  return key;
 	}
 
-		function reilencode(){
-  var message = AssignPlain();
-  var rail1=[];
-  var rail2=[];
-  var rail3=[];
-  message = message.toUpperCase().split(' ').join('');
-  for( var i = 0; i < message.length; i++ ){
-    if(i % 4 === 0 ){
-      rail1.push( message[i] );
-    }
-    else if ( i % 2 === 1  ){
-      rail2.push( message[i] );
-    }
-    else{
-      rail3.push( message[i] );
-    }
-  }
-  rail = rail1.join('') + rail2.join('') + rail3.join('');
-  document.getElementById("plain").value = rail;
-}
+  function reilencode()
+  {
+    ptext = AssignPlain().toUpperCase().replace(/[^a-zA-Z]/g, '');
+    key = AssignKey();
 
-function reildecode(){
-  var message = AssignPlain();
-  var ctr4 = 0;
-  var ctr3 = 0;
-  var ctr2 = 0;
-  var ctr1 = 0;
-  var rail4 = [];
-  var rail3 = [];
-  var rail2 = [];
-  var rail1 = [];
-  var rail = [];
-  var xmessage = message.toUpperCase().split(' ').join('');
-  for(i = 0; i < message.length; i++){
-    if(i % 4 === 0 )          ctr4++;
-    else if ( i % 4 === 3  )  ctr3++;
-    else if ( i % 4 === 2  )  ctr2++;
-    else                      ctr1++;
-  }
-  while(ctr4--)
-  {
-    rail4.push(xmessage[0]);
-    xmessage=xmessage.substring(1);
-  }
-  rail4=rail4.join('');
-  ctr0 = ctr1+ctr3;
-  for(i=0; i<ctr0; i++){
-    if(i%2==1)
-    {
-      rail3.push(xmessage[0]);
-      xmessage=xmessage.substring(1);
+    mainArray = Array(key);
+    for (i = 0; i < key; i++) {
+      mainArray[i] = Array(ptext.length);
+      for (s = 0; s < ptext.length; s++) {
+        mainArray[i][s] = '';
+      }
     }
-    else
-    {
-      rail1.push(xmessage[0]);
-      xmessage=xmessage.substring(1);    
+    j = 0;
+    r = 0;
+    for (i = 0; i < ptext.length; i++) {
+      p = ptext.substr(i, 1);
+      mainArray[j][i] = p;
+      
+      if (r == 0)       j = j + 1;
+      else if (r == 1)  j = j - 1;
+
+      if (j == key - 1) r = 1;
+      else if (j == 0)  r = 0;
     }
+    for (i = 0; i < mainArray.length; i++) {
+      mainArray[i] = mainArray[i].join('');
+    }
+
+    ctext = mainArray.join('');
+    document.getElementById("plain").value = ctext;
   }
-  rail1=rail1.join('');
-  rail3=rail3.join('');
-  while(ctr2--)
+
+  function reildecode()
   {
-    rail2.push(xmessage[0]);
-    xmessage=xmessage.substring(1);
+    ctext = AssignPlain().toUpperCase().replace(/[^a-zA-Z]/g, '');
+    key = AssignKey();
+
+    mainArray = Array(key);
+    for(i=0; i<key; i++)
+    {
+      mainArray[i] = Array(ctext.length);
+      for (s=0; s<ctext.length; s++)
+      {
+        mainArray[i][s] = "";
+      }
+    }
+    
+    q = 0;
+    for (t=0; t<mainArray.length; t++)
+    {
+      j = 0;
+      r = 0;
+      for (i=0; i<ctext.length; i++)
+      {
+        if (j == t)
+        {
+          c = ctext.substr(q,1);
+          mainArray[j][i] = c;
+          q = q + 1;
+        }
+        if (r == 0)       j = j + 1;
+        else if (r == 1)  j = j - 1;
+        
+        if (j == key - 1) r = 1;
+        else if (j == 0)  r = 0;
+      }
+    }
+    
+    j = 0;
+    r = 0;
+    ptext = "";
+    for (i=0; i<ctext.length; i++)
+    {
+      ptext = ptext + mainArray[j][i];
+      
+        if (r == 0)       j = j + 1;
+        else if (r == 1)  j = j - 1;
+        
+        if (j == key - 1) r = 1;
+        else if (j == 0)  r = 0;
+    }
+    document.getElementById("plain").value = ptext;
   }
-  rail2=rail2.join('');
-  for(i = 0; i < message.length; i++){
-    if(i % 4 === 0 ){
-      rail.push(rail4[0]);
-      rail4=rail4.substring(1);
-    }    
-    else if ( i % 4 === 1  ){
-      rail.push(rail1[0]);
-      rail1=rail1.substring(1);
-    }    
-    else if ( i % 4 === 3  ){
-      rail.push(rail3[0]);
-      rail3=rail3.substring(1);
-    }      
-    else{
-      rail.push(rail2[0]);
-      rail2=rail2.substring(1);
-    }    
-  }
-  var result ="";
-  for(i=0;i<rail.length;i++)
-  {
-    result = result.concat(rail[i]);
-  }
-  document.getElementById("plain").value = result;
-}
 	</script>
 </body>
 </html>
